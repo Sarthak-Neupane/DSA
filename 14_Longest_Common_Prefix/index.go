d3f16a053a2fbd4d2562df0b85da17b36082478d
Sarthak Neupane <sarthakrocks2004@gmail.com>
2024-08-27 14:38:23 -0500

    Completed number 14

---
+++ 14_Longest_Common_Prefix/index.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	children map[string]*Node
+	isEnd    bool
+}
+
+type Trie struct {
+	root *Node
+}
+
+func (t *Trie) Insert(w string) {
+	currentNode := t.root
+	for i := 0; i < len(w); i++ {
+		char := string(w[i])
+		if _, exists := currentNode.children[char]; !exists {
+			currentNode.children[char] = &Node{
+				children: make(map[string]*Node),
+				isEnd:    false,
+			}
+		}
+		currentNode = currentNode.children[char]
+	}
+	currentNode.isEnd = true
+}
+
+func initialize_trie() *Trie {
+	return &Trie{
+		root: &Node{
+			children: make(map[string]*Node),
+		},
+	}
+}
+
+func helper(strs []string) *Trie {
+	t := initialize_trie()
+	for _, v := range strs {
+		t.Insert(v)
+	}
+
+	return t
+}
+
+func longestCommonPrefix(strs []string) string {
+	t := helper(strs)
+	prefix := ""
+	currentNode := t.root
+
+	for len(currentNode.children) == 1 && !currentNode.isEnd {
+		for k := range currentNode.children {
+			prefix += k
+			currentNode = currentNode.children[k]
+		}
+	}
+
+	return prefix
+}
+
+func main() {
+	strs := []string{
+		"flower",
+		"flow",
+		"flight",
+		"flash",
+	}
+
+	fmt.Println(longestCommonPrefix(strs))
+}
---
+++ 69_Sqrt/index.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func mySqrt(x int) int {
+	return 1
+}
+
+func main() {
+	fmt.Println(mySqrt(32))
+}
